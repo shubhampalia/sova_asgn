394e23cd8f68c199de08f71701cce16129bfa9e5
shubham <shubhampalia26@gmail.com>
2021-01-30 13:12:15 +0530

    Add files via upload

---
+++ express_app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const upload = require('express-fileupload')
+const app = express()
+app.use(upload())
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/index.html')    
+})
+app.post('/', (req, res)=> {
+    if (req.files) {
+       console.log(req.files)
+       var file = req.files.file
+       var filename = file.name
+       console.log(filename)
+       const xlsx = require('xlsx')
+       var wb = xlsx.readFile('Book1.xlsx')
+       var ws = wb.Sheets["Sheet1"]
+       var data = xlsx.utils.sheet_to_json(ws)
+       console.log(data)
+                  
+       file.mv('./uploads/' + filename, function(err){
+           if (err){
+               res.send(err)
+           } else {
+               res.send(data)
+           }
+       })
+    }
+})
+app.listen(5001)
